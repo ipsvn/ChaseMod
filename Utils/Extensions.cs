4434fd99e673e5fdeb52e7854fd082bad762f1dc
svn <ipsvn@protonmail.com>
2024-07-13 23:06:08 +0100

    minor Extensions.cs cleanup

--- Utils/Extensions.cs
+++ Utils/Extensions.cs
@@ -28,8 +28,6 @@ public static class Extensions
     }
     public static void Set(this Vector thisVector, Vector3 vector2)
         => Set(thisVector, vector2.X, vector2.Y, vector2.Z);
-    public static void Set(this Vector thisVector, Vector vector2)
-        => Set(thisVector, vector2.X, vector2.Y, vector2.Z);
 
     public static Vector3 ToManaged(this Vector thisVector) => new Vector3(thisVector.X, thisVector.Y, thisVector.Z);
 
@@ -51,20 +49,11 @@ public static class Extensions
     
     public static void DisableUntil(this CBasePlayerWeapon weapon, int tick) => DisableUntil(weapon, tick, tick);
 
-    public static void DisableAllWeaponsUntil(this CPlayer_WeaponServices weaponServices, int tick)
-    {
-        foreach (var weaponHandle in weaponServices.MyWeapons)
-        {
-            if (!weaponHandle.IsValid) continue;
-            weaponHandle.Value?.DisableUntil(int.MaxValue);
-        }
-    }
-
     private static void SetMoveType(this CCSPlayerPawn pawn, MoveType_t moveType)
     {
         pawn.MoveType = moveType;
         Utilities.SetStateChanged(pawn, "CBaseEntity", "m_MoveType");
-        Schema.GetRef<MoveType_t>(pawn.Handle, "CBaseEntity", "m_nActualMoveType") = moveType;
+        pawn.ActualMoveType = moveType;
     }
     
     public static void FreezePlayer(this CCSPlayerPawn pawn) => pawn.SetMoveType(MoveType_t.MOVETYPE_OBSOLETE);
