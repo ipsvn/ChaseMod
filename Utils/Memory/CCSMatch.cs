2e4452191b22b65b8308ca008fef6983b0a1ad5c
svn <ipsvn@protonmail.com>
2025-02-20 12:25:08 +0000

    Remove UpdateTeamScores signature, replace with rewritten equivalent
    This fixes the plugin on Windows

--- Utils/Memory/CCSMatch.cs
+++ Utils/Memory/CCSMatch.cs
@@ -1,5 +1,7 @@
-﻿using CounterStrikeSharp.API.Core;
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Modules.Memory.DynamicFunctions;
+using CounterStrikeSharp.API.Modules.Utils;
 using System.Runtime.InteropServices;
 
 namespace ChaseMod.Utils.Memory;
@@ -8,16 +10,6 @@ public class CCSMatch
 {
     private static nint MATCH_OFFSET = 0xEE4;
 
-    // convar mp_default_team_winner_no_objective take func containing + 0xEE4 in param_1 and
-    // value 1 in param_2, follow the last function called taking param_1 (just hope this offset doesn't change :clueless:)
-    // TODO: I can probably remove this sig later
-    private static MemoryFunctionVoid<IntPtr> CCSMatch_UpdateTeamScores =
-        new(
-            ChaseModUtils.IsLinux
-                ? @"55 48 89 E5 41 57 41 56 41 55 49 89 FD BF 02 00 00 00"
-                : @"\x48\x89\x5C\x24\x2A\x48\x89\x74\x24\x2A\x48\x89\x7C\x24\x2A\x41\x56\x48\x83\xEC\x20\x48\x8B\xF9\xB9\x02\x00\x00\x00"
-        );
-    
     [StructLayout(LayoutKind.Sequential)]
     public struct MCCSMatch
     {
@@ -58,7 +50,39 @@ public class CCSMatch
         marshallMatch.m_terroristScoreTotal = marshallMatch.m_ctScoreTotal;
         marshallMatch.m_ctScoreTotal = temp;
 
+        UpdateTeamScores(marshallMatch);
         Marshal.StructureToPtr(marshallMatch, structOffset, true);
-        CCSMatch_UpdateTeamScores.Invoke(structOffset);
+    }
+
+    public static void UpdateTeamScores(MCCSMatch match)
+    {
+        var teams = Utilities.FindAllEntitiesByDesignerName<CCSTeam>("cs_team_manager");
+
+        var terrorists = teams.First(team => (CsTeam)team.TeamNum == CsTeam.Terrorist);
+        var cts = teams.First(team => (CsTeam)team.TeamNum == CsTeam.CounterTerrorist);
+
+        foreach(var team in teams)
+        {
+            var csTeam = (CsTeam) team.TeamNum;
+
+            if(csTeam == CsTeam.Terrorist)
+            {
+                team.Score = match.m_terroristScoreTotal;
+                team.ScoreFirstHalf = match.m_terroristScoreFirstHalf;
+                team.ScoreSecondHalf = match.m_terroristScoreSecondHalf;
+                team.ScoreOvertime = match.m_terroristScoreOvertime;
+            }
+            else if (csTeam == CsTeam.CounterTerrorist)
+            {
+                team.Score = match.m_ctScoreTotal;
+                team.ScoreFirstHalf = match.m_ctScoreFirstHalf;
+                team.ScoreSecondHalf = match.m_ctScoreSecondHalf;
+                team.ScoreOvertime = match.m_ctScoreOvertime;
+            }
+            Utilities.SetStateChanged(team, "CTeam", "m_iScore");
+            Utilities.SetStateChanged(team, "CCSTeam", "m_scoreFirstHalf");
+            Utilities.SetStateChanged(team, "CCSTeam", "m_scoreSecondHalf");
+            Utilities.SetStateChanged(team, "CCSTeam", "m_scoreOvertime");
+        }
     }
 }
